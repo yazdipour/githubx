aedd1b5b1983c4786451dd9a0ce0430c0e37d16b
Shahriar <shahriar.yazdipour@outlook.com>
2018-03-16 22:57:40 +0330

    Mute Error Video + Login With Cache if Offline

--- GithubX.UWP/GithubX.UWP/Views/LoginPage.xaml.cs
+++ GithubX.UWP/GithubX.UWP/Views/LoginPage.xaml.cs
@@ -13,18 +13,17 @@ namespace GithubX.UWP.Views
 
 		private async void Login_Click(object sender, RoutedEventArgs e)
 		{
-			if (!Services.Api.HttpHandler.CheckConnection) Frame.Navigate(typeof(ErrorPage));
 			LoadingControl.IsLoading = true;
-			await Login(accTextBox.Text.Trim());
-			LoadingControl.IsLoading = false;
-			async System.Threading.Tasks.Task Login(string acc)
+			try
 			{
-				var user = await Services.Api.ApiHandler.LoginAsync(acc);
+				var user = await Services.Api.ApiHandler.LoginAsync(accTextBox.Text.Trim());
 				if (user != null)
 					Frame.Navigate(typeof(StarListPage), user);
-				else
-					MainPage.NotifyElement.Show("Error! Try Again", 2000);
+				else if (!Services.Api.HttpHandler.CheckConnection) Frame.Navigate(typeof(ErrorPage));
+				else MainPage.NotifyElement.Show("Error! Try Again", 2000);
 			}
+			catch { }
+			LoadingControl.IsLoading = false;
 		}
 	}
 }
